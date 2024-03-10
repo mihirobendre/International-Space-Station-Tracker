4392ff80ed1c1b3fed43037930359b693b037920
Mihir Bendre <mihirobendre@utexas.edu>
2024-03-09 23:13:10 -0600

    Minor edits: pushing from VSCode to pull on TACC virtual machine

--- test_iss_tracker.py
+++ test_iss_tracker.py
@@ -1,7 +1,17 @@
 
-from iss_tracker import speed_calculator
+from iss_tracker import speed_calculator, fetch_all_data, get_stateVector, location_info, specific_epoch_speed, calculate_closest_datapoint_to_now
 
 import pytest
 
 def test_speed_calculator():
     assert speed_calculator(1,2,3) == 3.7416573867739413
+
+def test_fetch_all_data():
+    fetched_data = fetch_all_data()
+    assert len(fetched_data) == 1
+    assert isinstance(fetched_data[0], dict)
+
+
+
+
+
